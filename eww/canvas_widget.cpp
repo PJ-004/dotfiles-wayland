697d27909a7e1b3ccfc0a663146bd54a87b55c0a
PJ-004 <pranavjworkacc@gmail.com>
2025-06-12 19:02:19 -0700

    Reduced the number of assignments that appear

--- eww/canvas_widget.cpp
+++ eww/canvas_widget.cpp
@@ -100,25 +100,21 @@ int main() {
     result = curl_GET(base_url, auth_header);
     bool is_parsed = reader -> parse(result.c_str(), result.c_str() + result.length(), &jsonData, &errs);
     map<string, string> current_assignments = latest_assignments(jsonData);
-	/*
-    std::string yuck_output_string = R"x((box :orientation "vertical" :space-evenly false :spacing 5)x" + std::string("\n");
-    yuck_output_string += "    " + std::string(R"x((label :width 100 :text "Cultures of India Assignments: "))x") + "\n";
-    for (auto const& [name, due_date] : current_assignments) {
-        yuck_output_string += "        " + std::string(R"x((label :width 100 :text "Name - )x") + name + R"x("))x" + "\n";
-        yuck_output_string += "        " + std::string(R"x((label :width 100 :text "Due At - )x") + due_date + R"x("))x" + "\n\n";
-    }
-    yuck_output_string += ")";
 
-    std::cout << yuck_output_string << std::endl; // Still print to stdout
-    log_file << "Canvas script YUCK OUTPUT:\n" << yuck_output_string << std::endl;
-    log_file << "Canvas script finished at: " << std::time(nullptr) << std::endl;
-    log_file.close();
-	*/
+	// Change the iter value for the number of assignments wanted
+	int iter = 0;
+	int total_iter = 6;
+
+	// Generates the eww widget configuration
     cout << R"x((box :orientation "vertical" :space-evenly false :spacing 5)x" << flush; 
     cout << R"x((label :width 100 :text "Cultures of India Assignments: ")x" << ")" << flush;
     for (auto assignment: current_assignments) {
+		if (iter == total_iter)
+			break;
+
         cout << R"x((label :width 100 :text "Name - )x" << assignment.first << R"x("))x" << flush;
         cout << R"((label :width 100 :text "Due At - )" << assignment.second << R"x("))x" << flush;
+		++iter;
     }
     cout << ")" << flush;
 
